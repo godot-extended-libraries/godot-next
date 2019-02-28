7fa94ba2f79eb5f3ce99e3c72914e41dce35a8f3
xDGameStudios <xdgamestudios@gmail.com>
2019-02-28 20:50:42 +0000

    Add Behavior system Add PropertyInfo class Add ResourceCollection system

--- addons/godot-next/godot_next_plugin.gd
+++ addons/godot-next/godot_next_plugin.gd
@@ -1,2 +1,13 @@
 tool
 extends EditorPlugin
+
+const ResourceCollectionInspectorPlugin = preload("res://addons/godot-next/inspector_plugins/resource_collection_inspector_plugin.gd")
+
+var resource_collection_inspector_plugin
+
+func _enter_tree() -> void:
+	resource_collection_inspector_plugin = ResourceCollectionInspectorPlugin.new()
+	add_inspector_plugin(resource_collection_inspector_plugin)
+
+func _exit_tree() -> void:
+	remove_inspector_plugin(resource_collection_inspector_plugin)
