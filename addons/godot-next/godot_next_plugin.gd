925c35b94a3023521de9a75b03e2780b7e1bd5f5
xDGameStudios <xdgamestudios@gmail.com>
2019-03-12 16:15:13 +0000

    Implemented InspectorControls Rewrote ResourceCollection to use new DelegationInspectorPlugin Add Engine.editor_hint to CallbackDelegator Implemented Singleton system Added persistent singletons

--- addons/godot-next/godot_next_plugin.gd
+++ addons/godot-next/godot_next_plugin.gd
@@ -1,13 +1,13 @@
 tool
 extends EditorPlugin
 
-const ResourceCollectionInspectorPlugin = preload("res://addons/godot-next/inspector_plugins/resource_collection_inspector_plugin.gd")
+const DelegationInspectorPlugin = preload("res://addons/godot-next/inspector_plugins/delegation_inspector_plugin.gd")
 
-var resource_collection_inspector_plugin
+var delegation_inspector_plugin
 
 func _enter_tree() -> void:
-	resource_collection_inspector_plugin = ResourceCollectionInspectorPlugin.new()
-	add_inspector_plugin(resource_collection_inspector_plugin)
+	delegation_inspector_plugin = DelegationInspectorPlugin.new()
+	add_inspector_plugin(delegation_inspector_plugin)
 
 func _exit_tree() -> void:
-	remove_inspector_plugin(resource_collection_inspector_plugin)
+	remove_inspector_plugin(delegation_inspector_plugin)
