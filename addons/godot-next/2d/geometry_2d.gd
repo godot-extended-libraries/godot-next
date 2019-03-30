d18d85e1194b3a95c7a97df45fa4bacba3de41a1
Will Nations <willnationsdev@gmail.com>
2019-03-30 11:16:24 -0500

    Updated README / comments for Geometry2D.

--- addons/godot-next/2d/geometry_2d.gd
+++ addons/godot-next/2d/geometry_2d.gd
@@ -1,24 +1,26 @@
 # Geometry2D
 # author: Henrique "Pigdev" Campos
-"""
-Draws a Shape2D using CollisionShape2D's editor plugin's handles
----
-Notes
-
-Don't use it as direct child of CollisionBody2D classes unless you intent
-to use it as its CollisionShape2D.
-"""
-
+# license: MIT
+# description: Draws a Shape2D using CollisionShape2D's editor plugin handles.
+# notes:
+# - Don't use it as direct child of CollisionBody2D classes unless you intent
+#   to use it as its CollisionShape2D.
 tool
 extends CollisionShape2D
 class_name Geometry2D
 
+##### CLASSES #####
+
+##### SIGNALS #####
+
+##### CONSTANTS #####
+
+##### PROPERTIES #####
+
 export (Color) var color := Color(1, 1, 1, 1) setget set_color
 export (Vector2) var offset_position := Vector2(0, 0)
 
-func set_color(new_color: Color) -> void:
-	color = new_color
-	update()
+##### NOTIFICATIONS #####
 
 func _draw() -> void:
 	if shape is CircleShape2D:
@@ -28,6 +30,12 @@ func _draw() -> void:
 		draw_rect(rect, color)
 	elif shape is CapsuleShape2D:
 		draw_capsule(offset_position, shape.radius, shape.height, color)
+
+##### OVERRIDES #####
+
+##### VIRTUALS #####
+
+##### PUBLIC METHODS #####
 
 func draw_capsule(capsule_position: Vector2, capsule_radius: float,
 		capsule_height: float, capsule_color: Color) -> void:
@@ -41,3 +49,13 @@ func draw_capsule(capsule_position: Vector2, capsule_radius: float,
 	var rect_position: Vector2 = capsule_position - Vector2(capsule_radius, capsule_height * 0.5)
 	var rect := Rect2(rect_position, Vector2(capsule_radius * 2, capsule_height))
 	draw_rect(rect, capsule_color)
+
+##### PRIVATE METHODS #####
+
+##### CONNECTIONS #####
+
+##### SETTERS AND GETTERS #####
+
+func set_color(new_color: Color) -> void:
+	color = new_color
+	update()
