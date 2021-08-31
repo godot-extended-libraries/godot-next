c4c8e2afcbf1d8f352922750ced0d0b59b5c4e15
Wren <grahamgrasley@gmail.com>
2021-08-31 18:56:43 -0400

    Add files via upload

--- addons/godot-next/gui/back_buffer_panel.gd
+++ addons/godot-next/gui/back_buffer_panel.gd
@@ -1,13 +1,16 @@
 tool
 extends Control
 class_name BackBufferPanel, "res://addons/godot-next/icons/icon_back_buffer.png"
+# author: Tlitookilakin
+# Description: Copies its render region to the render buffer for shader purposes
 
-onready var rid = get_canvas_item()
+onready var rid = get_canvas_item() # get RID
 
 func _ready():
-	update()
+	update() #first-time update
 
 func _draw():
 	var rect: Rect2 = get_global_rect()
+	# screen-space coordinates are y-flipped
 	rect.position.y = get_viewport_rect().size.y - rect.position.y - rect.size.y
 	VisualServer.canvas_item_set_copy_to_backbuffer(rid,visible,rect)
